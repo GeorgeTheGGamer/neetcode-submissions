84240a762e309af311b0d2fe8eb0853d8f100bec
George Mahabir <147810913+GeorgeTheGGamer@users.noreply.github.com>
2026-09-16 17:26:04 +0100

    Add: surrounded-regions - submission-7

---
+++ Data Structures & Algorithms/surrounded-regions/submission-7.py
@@ -0,0 +1,44 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        # Goal is to start DFS from each cell on the edge and mark as not safe
+        # Then we clean up at the end
+
+        visited = set()
+        directions = ((0,1),(0,-1),(1,0),(-1,0))
+        rows, cols = len(board), len(board[0])
+
+        # Run Depth First Search
+        def traverse(i,j):
+            if (i,j) in visited:
+                return
+
+            board[i][j] = 'T'
+            visited.add((i,j))
+
+            for direction in directions:
+                next_i, next_j = i + direction[0], j + direction[1]
+                if 0 <= next_i < rows and 0 <= next_j < cols and board[next_i][next_j] == 'O':          
+                    traverse(next_i, next_j)
+        
+
+        # First we find all the O's that are bordering
+        for i in range(rows):
+            for j in range(cols):
+                if (i == 0 or j == 0 or i == rows-1 or j == cols-1) and board[i][j] == 'O':
+                    traverse(i,j)
+
+
+        # Next we change all the # to O and O to X
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'T':
+                    board[i][j] = 'O'
+                    continue
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+
+            
+
+        
