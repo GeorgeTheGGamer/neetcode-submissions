90aca7cc8a3ea3b6128b63a2c3dfc9a68a2eb0e3
George Mahabir <147810913+GeorgeTheGGamer@users.noreply.github.com>
2026-09-18 12:17:10 +0100

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # Undirected graph, we have the neighbours list -> do not need to have directed (graph question)
+        
+        # Hashmap to track the old to new node
+
+        old_to_new = {}
+
+        # Basic DFS
+        def dfs(node):
+            
+            if not node:
+                return None   
+            if node in old_to_new:
+                return old_to_new[node]
+            
+            copy = Node(node.val)
+            old_to_new[node] = copy
+
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+
+            return copy     
+
+        return dfs(node)   
+
+
+
+
+        
+
+
+        
