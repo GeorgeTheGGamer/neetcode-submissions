28a20ab457895b9e7b3372383b05d247b721dcc0
George Mahabir <147810913+GeorgeTheGGamer@users.noreply.github.com>
2026-09-18 14:23:04 +0100

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        # Tabulation: Each cell holds number of ways to get to that cell
+
+        if n < 2:
+            return n
+        prev=1            # 1 way to get to 1
+        curr = 2          # 2 ways to get to 2
+
+        for i in range(3, n+1):         # From index 3 to index n
+            prev, curr = curr, curr+prev
+        
+
+        return curr
+
+        
